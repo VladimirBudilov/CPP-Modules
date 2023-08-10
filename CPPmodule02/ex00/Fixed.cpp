6f091fd0a40c17d36136ce6db52a7f2eea9388e7
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-08-10 20:07:06 +0400

    add ex00

---
+++ CPPmodule02/ex00/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.h"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	_value = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void Fixed::setRawBits(const int raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_value = raw;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (&fixed != this)
+		_value = fixed.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value;
+}
