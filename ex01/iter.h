24c12ce46d66cfc99c36d64568892afdfe1ec8e7
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-08-22 13:08:21 +0400

    add ex00

---
+++ ex01/iter.h
@@ -0,0 +1,20 @@
+#ifndef C__CIRCLE_ITER_H
+#define C__CIRCLE_ITER_H
+
+#include "iostream"
+
+template <typename T>
+void iter(T *arr, size_t len, void (*f)(T&)) {
+	for (size_t i = 0; i < len; i++) {
+		f(arr[i]);
+	}
+}
+
+template <typename T, typename G>
+void iter(T *arr, size_t len,  void (*f)(G&)) {
+	for (size_t i = 0; i < len; i++) {
+		f(arr[i]);
+	}
+}
+
+#endif //C__CIRCLE_ITER_H
---
+++ ex01/main.cpp
@@ -0,0 +1,7 @@
+#include "iter.h"
+int main( void ) {
+	int arr[] = {1, 2, 3, 4, 5};
+	::iter(arr, 5, void { std::cout << a << std::endl; });
+
+	return 0;
+}
