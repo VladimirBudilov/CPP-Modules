b2972f970c3ab18e833a173176dfe62ebd7cf440
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-08-23 15:39:24 +0400

    ex02 in process

--- CPPmodule08/ex02/MutantStack.h
+++ CPPmodule08/ex02/MutantStack.h
@@ -1,8 +1,17 @@
-//
-// Created by Vladimir Budilov on 8/22/23.
-//
-
 #ifndef C__CIRCLE_MUTANTSTACK_H
 #define C__CIRCLE_MUTANTSTACK_H
+
+#include <iostream>
+#include "vector"
+#include "stack"
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	
+
+
+};
+
 
 #endif //C__CIRCLE_MUTANTSTACK_H
