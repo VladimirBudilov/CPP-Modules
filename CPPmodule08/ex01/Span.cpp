6b19fec0deb492528ee80c2b88c6946cc6828a87
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-08-23 15:07:15 +0400

    add ex01

--- CPPmodule08/ex01/Span.cpp
+++ CPPmodule08/ex01/Span.cpp
@@ -30,4 +30,40 @@ void Span::addNumber(int n)
 	_arr.push_back(n);
 }
 
+int Span::shortestSpan()
+{
+	if (_arr.size() <= 1)
+		throw std::out_of_range("Span is empty or has only one element");
+	std::vector<int> tmp = _arr;
+	std::sort(tmp.begin(), tmp.end());
+	int min = tmp[1] - tmp[0];
+	for (size_t i = 1; i < tmp.size(); i++)
+	{
+		if (tmp[i] - tmp[i - 1] < min)
+			min = tmp[i] - tmp[i - 1];
+	}
+	return min;
+}
+
+int Span::longestSpan()
+{
+	if (_arr.size() <= 1)
+		throw std::out_of_range("Span is empty or has only one element");
+	std::vector<int> tmp = _arr;
+	std::sort(tmp.begin(), tmp.end());
+	return tmp[tmp.size() - 1] - tmp[0];
+}
+
+Span::~Span()
+{
+
+}
+
+void Span::addNumber(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+	if (_arr.size() + std::distance(begin, end) > _N)
+		throw std::out_of_range("Span is full");
+	_arr.insert(_arr.end(), begin, end);
+}
+
 
