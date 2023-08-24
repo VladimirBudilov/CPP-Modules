675d7b626dc0c8daf6924e5da22749d3147e087f
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-08-24 19:28:56 +0400

    add templates

---
+++ CPPmodule09/ex02/PmergeMe.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vladimir Budilov on 8/24/23.
+//
+
+#include "PmergeMe.hpp"
---
+++ CPPmodule09/ex02/PmergeMe.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Vladimir Budilov on 8/24/23.
+//
+
+#ifndef C__CIRCLE_PMERGEME_HPP
+#define C__CIRCLE_PMERGEME_HPP
+
+
+class PmergeMe {
+
+};
+
+
+#endif //C__CIRCLE_PMERGEME_HPP
